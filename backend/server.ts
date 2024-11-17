5b056fe7722c01af60d2131b252cd7a8d2183b5b
nonaUmbak <nomundari.e1991@gmail.com>
2024-11-17 01:29:59 +0100

    added firebase admin lib and add function for send push notification

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,10 @@
 import { config } from 'dotenv';
 import express from 'express';
+import admin from 'firebase-admin';
 import { errorHandler } from './src/middlewares/errorHandler';
 import index from './src/routes/index';
+
+const serviceAccount = require('./path/to/serviceAccountKey.json');
 const app = express();
 
 //middlewares
@@ -13,6 +16,9 @@ app.use(errorHandler);
 config()
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 app.listen(process.env.PORT||1337);
 
 console.log(`Server on port ${process.env.PORT}`);
--- backend/src/service/notification_service.ts
+++ backend/src/service/notification_service.ts
@@ -1,4 +1,6 @@
+import admin from 'firebase-admin';
 import Notification from '../models/Notification';
+
 
 export async function getStudentNotifcations(studentId: number) {
     const data = await Notification.findAll({
@@ -17,3 +19,53 @@ export async function saveNotification(studentId, joinRequestId, notificationTyp
         })
         return result;
 }
+
+const getNotificationTemplate = (notificationType: string) => {
+    switch (notificationType) {
+        case 'joinRequest':
+            return {
+            title: 'New Join Request',
+            body: 'A student wants to join your group.',
+            };
+
+        case 'accept':
+            return {
+            title: 'Join Request Accepted',
+            body: 'Your request to join the group has been accepted!',
+            };
+
+        case 'reject':
+            return {
+            title: 'Join Request Rejected',
+            body: 'Your request to join the group has been rejected.',
+            };
+
+        default:
+            return {
+            title: 'Notification',
+            body: 'You have a new notification.',
+        };
+    }
+};
+
+export const sendPushNotification = async ( studentId: number, joinRequestId: number, token: string, notificationType: string) => {
+    try {
+        const template = getNotificationTemplate(notificationType);
+        if (!template) {
+            throw new Error(`Invalid notification type: ${notificationType}`);
+        }
+
+        const message = {
+            notification: template,
+            token,
+        };
+    
+        const response = await admin.messaging().send(message);
+        console.log('Notification sent successfully:', response);
+    
+        await saveNotification(studentId, joinRequestId, notificationType);
+        } catch (error) {
+        console.error('Failed to send push notification:', error.message);
+        }
+};
+
