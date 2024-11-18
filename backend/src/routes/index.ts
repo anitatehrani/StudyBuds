584c722ba621e391fd8c319db016fa4c467d241b
nonaUmbak <nomundari.e1991@gmail.com>
2024-11-18 10:40:29 +0100

    merged with main

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,15 +2,13 @@ import { Router } from 'express';
 export const router = Router();
 export default router;
 
-import {getAllStudents, getStudent} from '../controllers/studentController';
-
-import { basicSearchResult }  from '../controllers/basicSearchController';
 import { joinTheGroup } from '../controllers/joinrequestContoller';
-import { getStudentsAllNotification, saveStudentToken } from '../controllers/notificationController';
 import { getAllStudents, getStudent } from '../controllers/studentController';
+
+import { getStudentsAllNotification, saveStudentToken } from '../controllers/notificationController';
 router.get('/',getAllStudents);
 router.get('/:id', getStudent);
-router.get('/basic_search/:text/:student_id', basicSearchResult);
+// router.get('/basic_search/:text/:student_id', basicSearchResult);
 
 router.get('/notifcation/:studentId', getStudentsAllNotification);
 router.post('/notifcation', saveStudentToken);
