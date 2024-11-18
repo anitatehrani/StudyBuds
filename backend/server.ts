ede86ab4853c0c3dd83b65c268928eaf8a293181
nonaUmbak <nomundari.e1991@gmail.com>
2024-11-18 10:00:27 +0100

    commented the firebase config

--- backend/server.ts
+++ backend/server.ts
@@ -1,48 +1,9 @@
-// import { config } from 'dotenv';
-// import express from 'express';
-// import admin from 'firebase-admin';
-// import { errorHandler } from './src/middlewares/errorHandler';
-// import index from './src/routes/index';
-
-// import path from 'path';
-
-// const serviceAccount = require(path.resolve(process.cwd(), 'google-services.json'));
-
-// const app = express();
-
-// //middlewares
-// app.use(express.json());
-// app.use(express.urlencoded({extended:false}));
-// //routes
-// app.use(index);
-// app.use(errorHandler);
-// config()
-// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-// });
-// app.listen(process.env.PORT||1337);
-
-// console.log(`Server on port ${process.env.PORT}`);
-
 import { config } from 'dotenv';
 import express from 'express';
-import admin from 'firebase-admin';
 import { errorHandler } from './src/middlewares/errorHandler';
 import index from './src/routes/index';
 
 config();
-
-// const serviceAccount = require(path.resolve(process.cwd(), 'google-services.json'));
-// var admin = require("firebase-admin");
-
-var serviceAccount = require('../backend/studybuds-firebase.json');
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
 
 const app = express();
 
@@ -52,12 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(index);
 app.use(errorHandler);
 
+
+// const serviceAccountPath = path.resolve(__dirname, process.env.APP_CREDENTIALS || '');
+// if (!fs.existsSync(serviceAccountPath)) {
+//     throw new Error(`Service account file not found at: ${serviceAccountPath}`);
+// }
+
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-admin.initializeApp({
-    // credential: admin.credential.applicationDefault()
-    credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
