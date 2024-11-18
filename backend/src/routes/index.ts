cd42441b9ba18299b33f07f2d2e97fd9b5320164
Alexandru <araragicad@gmail.com>
2024-11-18 08:50:07 +0100

    Fixed at most one row result in sequelize + added env variables instead of some db parameters in clear + adapted some .ts files to the new db schema

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,7 +3,8 @@ export const router = Router();
 export default router;
 
 import {getAllStudents, getStudent} from '../controllers/studentController';
-import { basicSearchResult } from '../controllers/basicSearchController';
+
+import { basicSearchResult }  from '../controllers/basicSearchController';
 router.get('/',getAllStudents);
 router.get('/:id', getStudent);
 router.get('/basic_search/:text/:student_id', basicSearchResult);
