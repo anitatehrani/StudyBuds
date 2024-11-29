390f46eb0ab93604de323012187b1206a1ccfe4a
rikyiso01 <4943369@studenti.unige.it>
2024-11-29 11:24:10 +0100

    refactoring first models of the backend

--- backend/src/models/FbToken.ts
+++ backend/src/models/FbToken.ts
@@ -1,31 +1,41 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database';
+import { Column, Model, Table } from 'sequelize-typescript';
 
-const FbToken = sequelize.define(
-    'FbToken',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        token: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-        },
-        studentId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            field: 'student_id',
-        },
-    },
+@Table(
     {
         tableName: 'fb_token',
         schema: 'studybuds',
         timestamps: true,
         createdAt: 'created_at',
         updatedAt: 'updated_at',
-    }
-);
+    })
+class FbToken extends Model{
+    @Column(
+{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        }
+    )
+    id:number;
+    @Column(
+{
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        }
+    )
+    token:string;
+    @Column(
+{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: 'student_id',
+        }
+    )
+    studentId:number;
+}
+
+sequelize.addModels([FbToken])
 
 export default FbToken;
