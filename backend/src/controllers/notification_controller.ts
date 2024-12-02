d34b32f329d3e9ea397861fe3eb9694b32ff2dc1
Alexandru <araragicad@gmail.com>
2024-12-02 13:05:46 +0100

    add refactoring on some controller

--- backend/src/controllers/notification_controller.ts
+++ backend/src/controllers/notification_controller.ts
@@ -3,6 +3,7 @@ import Notification from '../models/Notification';
 import { getStudentFirebaseToken, saveFbToken, updateFbToken } from '../service/firebase_token_service';
 import { getStudentNotifications } from '../service/notification_service';
 import { ValidationError } from '../utils/api_error';
+import { checkInt, checkString, validateInt } from '../utils/validation_error';
 
 
 
@@ -14,30 +15,24 @@ export async function getAllNotification(req: Request, res: Response) {
 };
 
 export async function getStudentsAllNotification(req: Request, res: Response) {
-    const studentIdRaw=req.params['studentId']
-    if(studentIdRaw===undefined)throw new ValidationError("Missing Student ID")
-    const studentId=Number.parseInt(studentIdRaw);
-    if(isNaN())
-    const result = await getStudentNotifications(Number.parseInt(studentId))
+    const studentId = validateInt(req.params, "studentId");
+    const result = await getStudentNotifications(studentId)
     console.log(result)
     res.send(result)
 };
 
-export async function saveToken(req: Request, res: Response){
-    try {
-        const { studentId, token } = req.body;
+export async function saveToken(req: Request, res: Response) {
 
-        const fbTokenModel = await getStudentFirebaseToken(studentId);
+    const studentId = checkInt(req.body, "studentId");
+    const token = checkString(req.body, "token");
 
-        if (!fbTokenModel) {
-            await saveFbToken(studentId, token);
-            res.status(200).send({ message: "Successfully saved the student's Firebase token" });
-        } else {
-            await updateFbToken(studentId, token);
-            res.status(200).send({ message: "Successfully updated the student's Firebase token" });
-        }
-    } catch (err) {
-        console.error('Error in saveToken:', err);
-        res.status(err.status || 500).send(err.message || 'Internal server error');
+    const fbTokenModel = await getStudentFirebaseToken(studentId);
+
+    if (fbTokenModel === null) {
+        await saveFbToken(studentId, token);
+        res.send({ message: "Successfully saved the student's Firebase token" });
+    } else {
+        await updateFbToken(studentId, token);
+        res.send({ message: "Successfully updated the student's Firebase token" });
     }
 };
