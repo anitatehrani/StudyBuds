2ed5506bdcadfc80a31559ad02a876eacb2625c1
Lurpigi <luigiestebant@gmail.com>
2024-12-02 17:09:46 +0100

    refactor mockupdata and query

--- backend/src/mockup_data/groups.ts
+++ backend/src/mockup_data/groups.ts
@@ -1,11 +1,23 @@
-import { GroupMock } from"./models";
- 
-export const mockGroups = async () => {
-await GroupMock.bulkCreate([
-        { name: "Group A", course: "CSE", description: "This is a group for CSE students", members: 5, isPublic: true },
-        { name: "Group B", course: "ECE", description: "This is a group for ECE students", members: 10, isPublic: false },
-        { name: "Group C", course: "ME", description: "This is a group for ME students", members: 8, isPublic: true },
-    ]);
-};
-
-export { GroupMock };
+export const groupsData = [
+    {
+        name: "Group A",
+        course: "CSE",
+        description: "This is a group for CSE students",
+        members: 5,
+        isPublic: true,
+    },
+    {
+        name: "Group B",
+        course: "ECE",
+        description: "This is a group for ECE students",
+        members: 10,
+        isPublic: false,
+    },
+    {
+        name: "Another Group A",
+        course: "ME",
+        description: "This is another group for ME students",
+        members: 8,
+        isPublic: true,
+    },
+];
