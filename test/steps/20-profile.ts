b6496fa3fd55918341abf5874ee2ccc89258c2f7
Alexandru <araragicad@gmail.com>
2024-12-12 12:49:30 +0100

    add keys

--- test/steps/20-profile.ts
+++ test/steps/20-profile.ts
@@ -26,12 +26,47 @@ Given("I am on the profile page and logged in", async () => {
     await go_to_profile_page(driver);
 });
 
+Then(" I see my studentId, name, last name, telegram id", async function (String : string) {
+  const searchResults = byValueKey("search_results");
+
+    // Get the render object diagnostics for the ListView
+    const renderObjectDiagnostics = await driver.execute(
+      "flutter:getRenderObjectDiagnostics",
+      searchResults,
+      { includeProperties: true, subtreeDepth: 2 }
+    );
+
+    // Extract children count
+    const itemCount = renderObjectDiagnostics.children.length;
+    console.log(`Number of search results: ${itemCount}`);
+
+    for (let i = 0; i < itemCount; i++) {
+        console.log(`Checking search result ${i}...`);
+        const currentItem = byValueKey(`group_name_${i}`);
+        await driver.execute('flutter:waitFor', currentItem);
+        const itemText = await driver.getElementText(currentItem);
+        console.log(`Search result ${i}: ${itemText}`);
+
+        assert.ok(
+          itemText.toLowerCase().includes(String.toLowerCase()),
+          `Group name does not contain the text: ${String}`
+        );
+
+    }
+
+});
+
+
+
+
 // When("I type {string} in the search bar \\(case-insensitive)", async function(String : string) {
 //   const searchBar = byValueKey("search_bar");
 //   await driver.elementSendKeys(searchBar, String);
 //   const searchButton = byValueKey("search_button");
 //   await driver.elementClick(searchButton);
 // });
+
+
 
 // Then("I see all groups where {string} is inside their group name", async function (String : string) {
 
