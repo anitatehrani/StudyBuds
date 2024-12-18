d35bd54f2234f9c8f2bba60bfe2c5aae09568d96
Riccardo Isola <riky.isola@gmail.com>
2024-12-18 09:58:30 +0100

    add environment variable to control the flutter driver

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -5,12 +5,13 @@ import 'package:study_buds/firebase_options.dart';
 import 'package:study_buds/screens/login/login.dart';
 import 'package:study_buds/screens/main.dart';
 import 'package:study_buds/utils/push_notification.dart';
+import 'package:study_buds/utils/static_env.dart';
 
 const API_URL = String.fromEnvironment("API_URL",
     defaultValue: "http://34.154.87.170:5000");
 
 void main() async {
-  // enableFlutterDriverExtension();
+  if(DRIVER)enableFlutterDriverExtension();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   PushNotificationService.instance.retrievePushNotificationToken();
