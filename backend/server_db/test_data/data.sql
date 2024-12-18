ff3a619fb7900c48184e256f814c4c4b33d9a08e
Riccardo Isola <riky.isola@gmail.com>
2024-12-18 12:10:09 +0100

    change the mock data to fix the join private group error

--- backend/server_db/test_data/data.sql
+++ backend/server_db/test_data/data.sql
@@ -6,7 +6,7 @@ insert into student (student_id,telegram_account) values (4071780,33);
 insert into student (student_id,telegram_account) values (4669615,34);
 
 insert into student_group (id,name, description, course,admin_id, members_limit) values (100,'CP','description','Capstone',4071780, 10);
-insert into student_group (id,name, description, course,admin_id, members_limit) values (101,'CP2', 'description','Capstone',4669615, 10);
+insert into student_group (id,name, description, course,admin_id, members_limit,is_public) values (101,'CP2', 'description','Capstone',4669615, 10,false);
 
 insert into group_members (student_id,group_id) values (4071780,100);
 insert into group_members (student_id,group_id) values (4669615,100);
@@ -29,11 +29,11 @@ insert into student (student_id,telegram_account) values (12,37);
 insert into student_group (id,name, description,course,admin_id,members_limit) values (103,'joinrequest', 'description','Capstone',10,100);
 insert into group_members (student_id,group_id) values (10,103);
 
-insert into join_request (id,group_id,student_id,status) values (1,103,12,'pending');
-insert into notification (student_id,join_request_id,notification_type,message) values (10,1,'join_request','Nona has requested to join the Capstone project');
+insert into join_request (id,group_id,student_id,status) values (11,103,12,'pending');
+insert into notification (student_id,join_request_id,notification_type,message) values (10,11,'join_request','Nona has requested to join the Capstone project');
 
-insert into join_request (id,group_id,student_id,status) values (2,103,12,'pending');
-insert into notification (student_id,join_request_id,notification_type,message) values (10,2,'join_request','Nona has requested to join the Capstone project');
+insert into join_request (id,group_id,student_id,status) values (12,103,12,'pending');
+insert into notification (student_id,join_request_id,notification_type,message) values (10,12,'join_request','Nona has requested to join the Capstone project');
 
 
 -- Joined group list
@@ -46,6 +46,7 @@ insert into group_members (student_id, group_id) values (42674,104);
 
 insert into student_group (id,name,description, course,admin_id,members_limit) values (105,'groupof10','description','Capstone',10,100);
 insert into group_members (student_id,group_id) values (42674,105);
+insert into group_members (student_id,group_id) values (10,105);
 
 -- demo
 
