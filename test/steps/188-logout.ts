c7f807dbf0529b6d08da856edc00175b83207587
[your <anitatn78@gmail.com>
2025-01-08 14:09:57 +0100

    Changes in Tests for Logout Acceptance Testing and clearing cache of browser added

--- test/steps/188-logout.ts
+++ test/steps/188-logout.ts
@@ -10,22 +10,6 @@ import {
     clearChromeCacheFlutterCompatible
 } from "../utils/utils.ts";
 
-// Ensure the driver is available
-
-Given("I am logged in with credentials", async function () {
-    // Step 1: Wait for the login button and click it
-    console.log("Waiting for the login button to be visible...");
-    const loginButton = byValueKey("login_button");
-    await driver.execute("flutter:waitFor", loginButton);
-
-    console.log("Clicking the login button...");
-    await driver.elementClick(loginButton);
-
-    await login(driver, "10", "10");
-
-    console.log("Successfully logged in and returned to the app.");
-});
-
 Given("I am on the profile page", async function () {
     await go_to_page(driver, BottomBarIcon.profile);
 });
