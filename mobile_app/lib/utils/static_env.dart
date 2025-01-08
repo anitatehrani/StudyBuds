43e2f26a01daf5c129ff5c7b5b0b46a401a6490a
Lurpigi <luigiestebant@gmail.com>
2025-01-08 16:53:58 +0100

    update API_URL to new backend address

--- mobile_app/lib/utils/static_env.dart
+++ mobile_app/lib/utils/static_env.dart
@@ -1,4 +1,4 @@
 const API_URL = String.fromEnvironment("API_URL",
-    defaultValue: "http://34.154.87.170:5000");
+    defaultValue: "http://34.154.46.117:5000");
 
 const DRIVER = bool.fromEnvironment("DRIVER", defaultValue: false);
